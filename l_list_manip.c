98828da33c170f156f42f833bad9c70019ccace3
julietteknp <juliette.konop@gmail.com>
2026-05-18 12:53:24 +0200

    Add files via upload

--- l_list_manip.c
+++ l_list_manip.c
@@ -6,7 +6,7 @@
 /*   By: jkonop <jkonop@learner.42.tech>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 17:29:19 by jkonop            #+#    #+#             */
-/*   Updated: 2026/05/16 17:01:15 by jkonop           ###   ########.fr       */
+/*   Updated: 2026/05/18 12:48:34 by jkonop           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -34,15 +34,15 @@ void	add_back(t_stack **stack, t_stack *new)
 {
 	t_stack	*tmp;
 
-	if (*stack == NULL)
+	if (stack == NULL)
 	{
-		return 
-
+		*stack = new;
+		return ;
 	}
 	tmp = *stack;
 	while (tmp->next != NULL)
 		tmp = tmp->next;
-	tmp->next = tmp;
+	tmp->next = new;
 	new->next = NULL;
 }
 int	fake_atoi(char *argv)
@@ -77,6 +77,17 @@ t_stack	*last_node(t_stack *stack)
 	return (stack);
 }
 
+void    print_list(t_stack *stack)
+{
+	if (stack == NULL)
+		return ;	
+	while (stack != NULL)
+	{
+		printf("%d", stack->value);
+		stack = stack->next;
+	}
+}	
+
 int	main (int argc, char **argv)
 {
 	if (argc < 3)
@@ -84,17 +95,16 @@ int	main (int argc, char **argv)
 	argv++;
 	t_stack *init = new_node(5);
 	//printf("%d\n", init->value);
-	t_stack *start = init;
+	//t_stack *start = init;
 	while (*argv)
 	{	
 		t_stack *node = new_node(fake_atoi(*argv));
 		if (node == NULL)
 			return (1);
 		add_back(&init, node);
-		printf("%d\n", init->value);
 		argv++;
 	}
-	t_stack *last = last_node(start);
-	printf("%d", last->value);
+	//t_stack *last = last_node(start);
+	print_list(init);
 	return (0);
 }
